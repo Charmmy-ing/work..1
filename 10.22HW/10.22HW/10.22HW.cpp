3d010062c938634469b1bb38f3d8e3e15c0dadec
Charmmmy <2127137114@qq.com>
2025-11-13 15:23:11 +0800

    Add files via upload

---
+++ 10.22HW/10.22HW/10.22HW.cpp
@@ -0,0 +1,23 @@
+﻿#include<stdio.h>
+int main()
+{
+	int e=0, w=0, n=0, o=0;
+	int c;
+	printf("PLEASE ENTER:");
+	while((c=getchar())!='\n')
+	{
+		if (c>='A' && c <= 'Z' || (c>='a' && c <= 'z'))
+			e++;
+		else
+			if (c == ' ')
+				w++;
+			else
+				if (c <= '9' && c >= '0')
+					n++;
+				else
+					o++;
+	}
+	printf("%d,%d,%d,%d", e, w, n, o);
+	printf("\n");
+	return 0;
+}
---
+++ 10.24homework/10.24homework/10.24homework.c
@@ -0,0 +1,29 @@
+﻿#include<stdio.h>
+int main()
+{
+	int i,integer=2;
+	int j, sign,l=0;
+	while(integer < 200)
+	{
+			sign = 1;
+			for (j = 2; j <integer; j++)
+			{
+				if (integer % j == 0)
+				{
+					sign=0;
+					break;
+				}
+			}
+			if (sign)
+			{
+				printf("%d\t", integer);
+				l++;
+				if (l % 5 == 0)
+				{
+					printf("\n");
+				}
+			}
+			integer++;
+	}
+	return 0;
+}
